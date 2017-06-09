7b72f52e54c15dbf9ddb93e48809c1a5d9ab1e85
Ben Elam <ben.a.elam@gmail.com>
2017-06-08 19:33:15 -0700

    Isolate arg parsing logic in greprs

--- examples/greprs/src/main.rs
+++ examples/greprs/src/main.rs
@@ -11,8 +11,7 @@ fn main() {
     // :? is the debug formatter
     println!("{:?}", args);
 
-    let query = &args[1];
-    let filename = &args[2];
+    let (query, filename) = parse_config(&args);
 
     println!("Searching for {}", query);
     println!("In file {}", filename);
@@ -24,4 +23,11 @@ fn main() {
         .expect("something went wrong reading the file");
 
     println!("With text:\n{}", contents);
+}
+
+fn parse_config(args: &[String]) -> (&str, &str) {
+    let query = &args[1];
+    let filename = &args[2];
+
+    (query, filename)
 }
