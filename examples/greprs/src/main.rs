4945503574e03836753eaa53cfd1405981135845
Ben Elam <ben.a.elam@gmail.com>
2017-06-08 20:58:26 -0700

    Handle parser errors

--- examples/greprs/src/main.rs
+++ examples/greprs/src/main.rs
@@ -2,6 +2,7 @@ use std::env;
 use std::fs::File;
 // Traits for IO
 use std::io::prelude::*;
+use std::process;
 
 fn main() {
     // Call .collect() to exhaust the iterator
@@ -11,7 +12,10 @@ fn main() {
     // :? is the debug formatter
     println!("{:?}", args);
 
-    let config = Config::new(&args);
+    let config = Config::new(&args).unwrap_or_else(|err| {
+        println!("Problem parsing arguments: {}", err);
+        process::exit(1);
+    });
 
     println!("Searching for {}", config.query);
     println!("In file {}", config.filename);
@@ -31,13 +35,16 @@ struct Config {
 }
 
 impl Config {
-    fn new(args: &[String]) -> Config {
+    fn new(args: &[String]) -> Result<Config, &'static str> {
+        if args.len() < 3 {
+            return Err("not enough arguments");
+        }
         let query = args[1].clone();
         let filename = args[2].clone();
 
-        Config {
+        Ok(Config {
             query: query,
             filename: filename
-        }
+        })
     }
 }
